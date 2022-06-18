9c99627de42bb751477e52cbb1505b22c145d905
Gustavo Koch <guzko@hotmail.com>
2022-06-18 17:30:39 +0200

    started Routers

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+const postsRouter=require('/posts.js');
+app.use('/posts', postsRouter);
+
+const authorsRouter=require('/author.js');
+app.use('/authors', authorsRouter);
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
---
+++ posts.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const postsRouter = express.Router();
+const posts = require("./posts.js");
+
+postsRouter.get("/:id", (req, res, next) => {
+  const post = posts[req.params.id];
+  if (post) {
+    res.send(post);
+  } else {
+    res.status(404).send();
+  }
+});
+
+module.exports = postsRouter;
